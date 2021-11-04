98c535fcf05d25c8c3ef69a95b0b73b6536d1bfc
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-04 10:10:37 -0300

    Concatenaçao
    Explicaçao sobre a concatenaçao no python, onde podemos utilizar com o sinal de + ou a virgula.

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-1.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-1.py
@@ -9,3 +9,16 @@ print("Hello Word! My name is Maria Clara. This is my college's number one exerc
 print(2+3) #Quando não colocamos aspas nos números, realizamos uma operação. 
 
 print("2+3") #Quando adicionamos aspas, tornamos ela uma string, então neste caso, será retornado 2+3, não o resultado da operação.s
+
+#CONCATENAÇÃO: 
+#A concatenação ocorre quando juntamos dois valores. 
+
+print("2" + "3") #Este valor será retornado como: 23, pois usamos aspas e o sinal de +, que neste caso, juntou os dois números. 
+
+print("Hello, " + "Word!") #Aqui estamos concatenando mensagens, nota-se que usamos as aspas e o + como o caso acima. 
+
+print("Hello, " , "Word!") #Neste caso, ao invés do + utilizamos a vírgula. Sendo assim, podemos usar tando o + quanto a vírgula para concatenar mensagens e números. 
+ #Ambos os resultados darão "Hello, Word!"
+
+print("O resultado da soma de 2 e 3 é:" , 2+3) #Aqui concatenamos números e texto. Observe que usamos a virgula para realizar a concatenação. 
+#O resultado será: "O resultado da soma de 2 e 3 é: 5"
