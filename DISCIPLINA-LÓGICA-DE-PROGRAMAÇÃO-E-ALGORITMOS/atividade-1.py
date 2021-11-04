05b59da205294807ccfacda7d6ad9ce0ee80914f
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-04 09:38:24 -0300

    Operaçoes Aritmeticas
    Add exemplos da realizaçao de operaçoes aritmeticas e o uso das aspas em numeros

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-1.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-1.py
@@ -4,3 +4,8 @@
 
 print("Hello Word! My name is Maria Clara. This is my college's number one exercise! I am a Software Engineering student at Uninter College!")
 
+#OPERAÇÕES ARITMÉTICAS:
+
+print(2+3) #Quando não colocamos aspas nos números, realizamos uma operação. 
+
+print("2+3") #Quando adicionamos aspas, tornamos ela uma string, então neste caso, será retornado 2+3, não o resultado da operação.s
