31edcf05a327909964116091a9fb2230635348ad
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-04 09:27:40 -0300

    Add funçao print:
    Add funçao print e concertando frase em ingles

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-1.py
@@ -0,0 +1,6 @@
+#ATIVIDADE/PRÁTICA - CICLO DE PROCESSO DE DADOS 
+
+#O professor mostra como utilizar a função print e sua sintaxe:
+
+print("Hello Word! My name is Maria Clara. This is my college's number one exercise! I am a Software Engineering student at Uninter College!")
+
