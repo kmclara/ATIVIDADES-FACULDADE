8eed8c58dd5e3f866a7e690d1f4a06ceb263d2ec
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-09 09:01:40 -0300

    Explicaçao e Exemplos
    Expplicaçao sobre Dados e Variaveis e exemplos de variaveis.

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-2.py
@@ -0,0 +1,13 @@
+#VARIÁVEL, DADOS E SEUS TIPOS. 
+
+#Dado é qualquer coisa que podemos manipular em nosso programa. 
+#Variável é um conjunto de endereços que estão na memória, conjunto que possui dados. 
+
+#Atribuição: Na atribuição é quando criamos uma variável e atribuimos ela um valor com o símbolo de "="
+#Quando atribuimos uma valor á variável, colocamos aquele valor na memória. 
+
+#Exemplo:
+NotaDaDisciplina = 10.0
+Disciplina = "Lógica de Programação e Algoritmos"
+
+print(NotaDaDisciplina, Disciplina)
