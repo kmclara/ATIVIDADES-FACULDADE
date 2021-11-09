1e394311ada7456d4983ae0cf2efacba4d7143a1
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-09 09:34:14 -0300

    Explicaçao e Exemplos sobre as regras de uma variavel
    Foi adicionado explicaçao e exemplos das regras de uma variavel

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-2.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-2.py
@@ -14,3 +14,37 @@ Disciplina = "Lógica de Programação e Algoritmos"
 #Lê-se: A variável "Disciplina" recebe o dado "Lógica de Programação e Algoritmos"
 
 print(NotaDaDisciplina, Disciplina)
+
+#Regras de Variáveis
+
+print("Regra 1: Não comece uma variável com números! O Python não irá reconhecer!")
+
+#3abc = #Não reconhece! Irá dar erro!
+
+print("Viu? Não reconhece! Mas podemos usar números no meio da variável!")
+
+Variável1 = "Agora essa deu!"
+
+print(Variável1)
+
+print("Regra 2: Inicie um variável com letra ou sublina (_)! ")
+
+DiaDaSemana = "Começando com letras"
+_DiaDaSemana = "Começando com sublinha"
+
+print("As variáveis {} e {} deram certo!".format(DiaDaSemana, _DiaDaSemana))
+
+print("Regra 3: Números, letras e sublinhas podem ser colocadas á vontade no meio da variável!")
+
+DiaDaSemana2 = "Variável com número no meio"
+Dia_Da_Semana_ = "Variável com sublinha no meio"
+
+print(Dia_Da_Semana_, DiaDaSemana2)
+print("Podemos ver que deu certo!")
+
+print("Observação: Não podemos usar exclamção, interrogação, números, espaço, & e nada no incio das nossas variáveis!")
+print("Apenas letras e sublinha são permitidas para colocar no inicio das nossas variaveis!")
+
+#Exemplo ERRADO de variável iniciada com exclamação:
+#!Nota = 10.0
+#Isso está ERRADO! Irá dar erro assim como deu quando colocamos número no inicio da nosso variável!
