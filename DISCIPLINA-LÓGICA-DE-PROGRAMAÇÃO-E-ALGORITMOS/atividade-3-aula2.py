cf1e535f86ac8737a872e28d85718e91763eb85d
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-11 09:15:56 -0300

    Add funçao print
    Adicionado funçao print informando as idades coletadas e postas pelo usuario
    Adicionado funçao print informando o resultado da soma de ambas as idades

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-3-aula2.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-3-aula2.py
@@ -10,3 +10,6 @@ IdadePai = int(input("Digite a idade do pai: "))
 #Realizando a soma das idades: 
 SomaDasIdades = (IdadeFilho+IdadePai)
 
+#Resultado do programa:
+print("Olá! As idades informadas foram: Filho - {}, Pai - {}.".format(IdadeFilho, IdadePai))
+print("A soma de ambas foi de {}! Obrigada por usar nosso programa!".format(SomaDasIdades))
