4f03ed9ee2345fe31b07f9d3a9bef0eae5bff874
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-11 11:02:48 -0300

    Adicionando funçao print
    Adicionando funçao print com a borda para separar as atividades.

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade4-aula2-prática.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade4-aula2-prática.py
@@ -38,3 +38,5 @@ print("O mod irá nos retornar o resto da divisão, ou seja, a sobra do mesmo.")
 
 SobraDaDivisão = (403%73)
 print("A sobra da divisão entre 403 e 73 é {}!".format(SobraDaDivisão))
+
+print("_*_" *30)
