1efff29cb91456e5a02192fb5aa51a7d51ff79ec
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-11 10:22:34 -0300

    Add funçao print
    Adicionado funçao print para indicar o primeiro exercicio proposto pelo educador

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade4-aula2-prática.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade4-aula2-prática.py
@@ -1,3 +1,8 @@
 #Atividade 1 - Expressões Algébricas 
 
 print("Exercicios de prática sobre a Aula 2 de Lógica de Programação e Algoritmos.")
+
+#A - O somátorio dos 5 primeiros números inteiros positivos. 
+print("Os 5 primeiros números inteiros positivos são: 1, 2, 3, 4 e 5.")
+print("Somatório dos números: ")
+
