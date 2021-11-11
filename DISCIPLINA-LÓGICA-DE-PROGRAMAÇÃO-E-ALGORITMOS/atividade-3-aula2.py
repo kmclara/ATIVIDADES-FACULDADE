2d240de5e48a2b1daa532deb02567c7434aec4a4
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-11 09:11:20 -0300

    Adicionando print e variaveis
    Add print para informar ao usuario do que se trata o programa e seu intuito.
    Add as duas variaveis para coletar as idades dos usuarios para realizar o processo

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-3-aula2.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-3-aula2.py
@@ -1,3 +1,8 @@
 #Desenvolver um algoritmo que solicite ao usuário dois números inteiros. Imprima a soma destes dois números
 #na tela!
 
+#Solicitando ao usuário dois números:
+print("Para saber a soma de pai e filho, responda as questões abaixo, e aguarde o resultado!")
+
+IdadeFilho = int(input("Digite a idade do filho: "))
+IdadePai = int(input("Digite a idade do pai: "))
