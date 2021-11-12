3858b9ab45b404b15eebed7309adef10d39c5742
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-12 13:47:46 -0300

    Adicionando espaço
    Adicionando espaço depois da palavra Programaçao na variavel String1, pois estava se juntando com o - e - da String2.

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-3-manipulaçãodestrings.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-3-manipulaçãodestrings.py
@@ -8,7 +8,7 @@ print("Na concatenação de strings, fazemos a junção ou soma de duas strings.
 print("Se usa o sinal de + para realizar a concatenação!")
 print("Exemplo: ")
 
-String1 = "Lógica de Programação"
+String1 = "Lógica de Programação "
 String2 = String1 + "e Algoritmos"
 print("A concatenação se resultará em: {}".format(String2))
 
