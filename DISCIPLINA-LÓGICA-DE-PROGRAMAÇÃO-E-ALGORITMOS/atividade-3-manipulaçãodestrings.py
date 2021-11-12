2fce34ad107ae6bbe5873fe7e1ec16bac08fb519
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-12 13:44:42 -0300

    Adicionando variaveis e funçao print
    Adicionando duas variaveis para realizar a concatenaçao de string
    Adicionando a funçao print para demostrar o resultado da concatenaçao

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-3-manipulaçãodestrings.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade-3-manipulaçãodestrings.py
@@ -7,3 +7,7 @@ print("_*_" *30)
 print("Na concatenação de strings, fazemos a junção ou soma de duas strings.")
 print("Se usa o sinal de + para realizar a concatenação!")
 print("Exemplo: ")
+
+String1 = "Lógica de Programação"
+String2 = String1 + "e Algoritmos"
+print("A concatenação se resultará em: {}".format(String2))
