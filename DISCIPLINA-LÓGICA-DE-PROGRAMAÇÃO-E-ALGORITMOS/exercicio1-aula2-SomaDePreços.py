0d85ebc0c9918f63cc54a83babc6d2e341459066
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-16 10:20:34 -0300

    Adicionando funçao print
    Adicionando funçao print para o começo do exercicio e a borda para separar o restante que ira vir

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/exercicio1-aula2-SomaDePreços.py
@@ -0,0 +1,5 @@
+#Exercício1: Desenvolver um algoritmo que solicite ao usuário o preço de um produto e um percentual
+#de desconto a ser aplicado a ele. Calcule e exiba o valor do desconto e o preço final do produto.
+
+print("Começando o exercício 1!")
+print("_*_" *30)
