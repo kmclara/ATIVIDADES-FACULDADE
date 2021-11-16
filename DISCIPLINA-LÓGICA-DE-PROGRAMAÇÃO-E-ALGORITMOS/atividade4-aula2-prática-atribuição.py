90fbc5bfd2882e2280254647cebd21118b76685f
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-16 09:04:11 -0300

    Adicionando funçao print e variavel
    Adicionando funçao print para demostrar o que ira se passar no exercicio e explicando sobre o que e a atribuiçao
    Adicionando variavel para dar a atribuiçao
    Adicionando funçao print vazia.

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade4-aula2-prática-atribuição.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividade4-aula2-prática-atribuição.py
@@ -2,3 +2,10 @@
 
 print("Exercicios práticos da Aula 2. Exercicios sobre Atribuição!")
 
+#Atividade 1: Atribuir o valor 3 na variavel a.
+print("Aqui iremos atribuir o valor 3 na variavel a.")
+print("O sinal de atribuição no Python é =.")
+
+A = 3
+
+print("")
