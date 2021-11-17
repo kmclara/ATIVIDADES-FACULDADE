c50235d6f09a9a6794d7a36da2c4c871a86700b0
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-17 09:12:45 -0300

    Retirando uma variavel e mudando comentario

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/exercicio3-aula2-String.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/exercicio3-aula2-String.py
@@ -5,6 +5,5 @@
 print("Começando Exercicio 3!")
 print("_*_" *30)
 
-#Criando as variáveis com frases aleatórias que se completam.
+#Criando as variáveis com frases aleatórias.
 String1 = input("Informe uma frase: ")
-String2 = input("Informe outra frase: ")
