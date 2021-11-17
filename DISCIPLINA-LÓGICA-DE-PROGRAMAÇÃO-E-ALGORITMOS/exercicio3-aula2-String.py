66e05ac598da65b7ff409e894f4b1b021456cb83
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-17 08:53:51 -0300

    Adicionando funçao print
    Adicionando funçao print para dar inicio ao exercicio
    Adicionando funçao para adicionar a borda para a separaçao das atividades

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/exercicio3-aula2-String.py
@@ -0,0 +1,6 @@
+#Exercicio 3 - Crie uma variável de string que receba uma frase qualquer. Crie uma segunda variável, 
+#agora contendo a metade da string digitada. Imprima na tela somente os dois últimos caracteres
+#da segunda variável do tipo string. 
+
+print("Começando Exercicio 3!")
+print("_*_" *30)
