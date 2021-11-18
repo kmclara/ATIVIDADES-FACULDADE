58e83fe0fd6ec29cd7e869e358764496f9ea8e0b
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-18 09:05:59 -0300

    Renomeando arquivo

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/ativiade1prática-aula3-condições.py
@@ -0,0 +1,17 @@
+#Atividade prática 1: Desenvolva um programa que lê dois valores numéricos
+#inteiros e compara se o primeio é maior do que o segundo, utilizando uma 
+#condicional simples. Caso seja (resultado verdadeiro), ele imprime na tela
+#a mensagem informando que o primeiro valor digitado é maior do que o segundo.
+
+print("ATIVIDADE 1 - ATIVIDADE PRÁTICA - AULA 3 - CONDIÇÃO SIMPLES")
+print("_*_" *30)
+
+#Pedindo ao usuário dois valores inteiros númericos: 
+ValorInteiroNumero1 = int(input("Digite o primeiro valor númerico inteiro: "))
+ValorInteiroNumerico2 = int(input("Digite o segundo valor númerico inteiro: "))
+
+#Comparando se o primeiro valor é maior que o segundo valor usando uma condicional simples: 
+if ValorInteiroNumero1 > ValorInteiroNumerico2: 
+    print("O número {} é maior que o número {}!".format(ValorInteiroNumero1, ValorInteiroNumerico2))
+
+print("_*_" *30)
