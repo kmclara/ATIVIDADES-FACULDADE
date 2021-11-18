7d1e5a9bd6ec56e320f8177b2adec66a63f3ef44
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-18 10:48:23 -0300

    Comentando atividade 1

--- DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/ativiade1prática-aula3-condições.py
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/ativiade1prática-aula3-condições.py
@@ -7,12 +7,12 @@ print("ATIVIDADE 1 - ATIVIDADE PRÁTICA - AULA 3 - CONDIÇÃO SIMPLES")
 print("_*_" *30)
 
 #Pedindo ao usuário dois valores inteiros númericos: 
-ValorInteiroNumero1 = int(input("Digite o primeiro valor númerico inteiro: "))
-ValorInteiroNumerico2 = int(input("Digite o segundo valor númerico inteiro: "))
+#ValorInteiroNumero1 = int(input("Digite o primeiro valor númerico inteiro: "))
+#ValorInteiroNumerico2 = int(input("Digite o segundo valor númerico inteiro: "))
 
 #Comparando se o primeiro valor é maior que o segundo valor usando uma condicional simples: 
-if ValorInteiroNumero1 > ValorInteiroNumerico2: 
-    print("O número {} é maior que o número {}!".format(ValorInteiroNumero1, ValorInteiroNumerico2))
+#if ValorInteiroNumero1 > ValorInteiroNumerico2: 
+ #   print("O número {} é maior que o número {}!".format(ValorInteiroNumero1, ValorInteiroNumerico2))
 
 print("_*_" *30)
 
