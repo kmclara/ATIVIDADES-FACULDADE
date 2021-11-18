b537dbf0e1edac2cccfcc6932270ea950023abfc
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-18 08:43:42 -0300

    Adicionando comentario e funçao print
    Adicionando comentario do enunciado do exercicio
    Adicionando funçao print com o titulo e borda de separaçao

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/ativiade1prática-aula3-condiçãosimples.py
@@ -0,0 +1,7 @@
+#Atividade prática 1: Desenvolva um programa que lê dois valores numéricos
+#inteiros e compara se o primeio é maior do que o segundo, utilizando uma 
+#condicional simples. Caso seja (resultado verdadeiro), ele imprime na tela
+#a mensagem informando que o primeiro valor digitado é maior do que o segundo.
+
+print("ATIVIDADE 1 - ATIVIDADE PRÁTICA - AULA 3 - CONDIÇÃO SIMPLES")
+print("_*_" *30)
