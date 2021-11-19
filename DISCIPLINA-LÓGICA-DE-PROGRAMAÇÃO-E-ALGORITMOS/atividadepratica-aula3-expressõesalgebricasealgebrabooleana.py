93e6a61c31db8bbb95c2f907fcfc6538d8506d50
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-19 09:16:54 -0300

    Adicionando comentario e funçao print
    Adicionando comentario com o enunciado da atividade
    Adicionando funçao print com o titulo da atividade e borda para separar as atividades

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividadepratica-aula3-expressõesalgebricasealgebrabooleana.py
@@ -0,0 +1,6 @@
+#ATIVIDADE PRÁTICA: Um aluno, para passar de ano, precisar ser aprovado em todas as matérias que está cursando.
+#Assuma que a média para aprovação é a partir de 7, e que o aluno cursa 3 matérias, somente. Escreva um algoritmo
+#que leia  nota final do aluno em cada matéria e informe na tela, se ele passou de ano ou não.
+
+print("ATIVIDADE PRÁTICA - AULA 3 - NOTA DO ALUNO")
+print("_*_" *30)
