d0fd61f3b6a22b7fe7a3dc33c331bd4179a97658
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-20 14:46:09 -0300

    Adicionando funçao print
    Adicionando funçao print com o titulo da atividade
    Adicionando funçao print com a borda para separar a atividade.

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividadepratica-aula3-condiçõesaaninhadas.py
@@ -0,0 +1,8 @@
+#Atividade prática: Escreva um algoritmo em python em que o usuário escolhe se quer comprar maçãs,
+#laranjas ou bananas. Deverá ser apresentado na tela um menu com as opções: 1- Maçã, 2- Laranja e 3- Banana
+#Após escolhida a fruta, deve-se digitar a quantidade que se quer comprar. O algoritmo deve calcular
+#o preço total a pagar do produto escolhido e mostra-lo na tela. Considere uma maçã custa R$2.30, uma laranja 3.60 e
+#uma banana 1.85
+
+print("ATIVIDADE PRÁTICA - AULA 3 - CONDIÇÕES ANINHADAS - COMPRA DE FRUTAS!")
+print("_*_" *30)
