b67393cd7287664401abfa3732b4a64eedf12a80
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-20 15:42:11 -0300

    Adicionando funçao print
    Adicionando funçao print com o titulo da atividade
    Adicionando funçao print com a borda para separar as atividades

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/atividadepratica-aula3-condiçãoelif.py
@@ -0,0 +1,7 @@
+#Atividade Prática: Escreva um algoritmo que lê um nome e uma idade. Caso o nome digitado seja Vinicius,
+#escreva isso na tela. Caso o usuário digite qualquer outro nome, verifique sua idade. Se for menor que 18
+#anos, informe que é de menor. Se for maior que 100 anos, informe que está pessoa possivelmente
+#não existe.
+
+print("ATIVIDADE PRÁTICA - AULA 3 - CONDICIONAIS DE MÚLTIPLA ESCOLHA - ELIF")
+print("_*_" *30)
