cd8f073d8bf46772a4b11d03c75f7ef37de50aef
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-22 10:21:57 -0300

    Apenas testes com a estrutura de repetiçao while

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/teste-while.py
@@ -0,0 +1,18 @@
+x = 1
+while (x <= 5):
+    print(x)
+    x=x+1
+
+#x = 0 
+#while(x <= 99):
+  #  print(x)
+   # x=x+1
+
+soma = 0
+contagem = 1 
+while (contagem <= 5):
+  x = float(input("Digite o nota {}".format(contagem)))
+  soma +=x #Equivale a soma = x+1
+  contagem +=1 #Equivale a contagem = x+1
+print("Somatório é {}".format(soma))
+
