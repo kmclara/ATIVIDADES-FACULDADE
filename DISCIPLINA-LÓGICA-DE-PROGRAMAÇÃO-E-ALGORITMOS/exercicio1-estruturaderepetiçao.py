6c6db45f4781a5aec7ce88163c8fa4c4586808fb
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-23 09:05:26 -0300

    Adicionando funçao print
    Adicionando funçao print com o titulo da atividade
    Adicionando funçao print com a borda de separaçao da atividade

---
+++ DISCIPLINA-LÓGICA-DE-PROGRAMAÇÃO-E-ALGORITMOS/exercicio1-estruturaderepetiçao.py
@@ -0,0 +1,6 @@
+#EXERCICIO 1: Realize a sequência de print com for e while.
+#A: Inteiros de 3 até 12, incluso o 12.
+#B: Inteiros de 0 até 9, excluindo 9, com passo de 2.
+
+print("ATIVIDADES DE LAÇOS DE REPETIÇÃO!")
+print("_*_" *30)
