991dfc485c6b088dfa28c5332ed8be99e153954b
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-23 14:36:57 -0300

    Nome modificado

---
+++ DISCIPLINA-LPA/exercicio2-estruturaderepeticao.py
@@ -0,0 +1,8 @@
+#Exercicio 2: Escreva um algoritmo que leia um valor e que imprima a quantidade de cédulas necessárias
+#para pagar esse mesmo valor. Para simplificar, vamos trabalhar apenas com valores inteiros
+#e com cédulas de R$100, R$50, R$10, R$20, R$5 e R$1.
+
+print("EXERCICIO 1 - AULA PRÁTICA")
+print("CAIXA ELETRÔNICO")
+print("___" *30)
+
