f82341d8dc9a9a171eeb975ae7e415bbaa711146
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-11-24 09:32:28 -0300

    adicionando funçao print com borda de separacao

---
+++ DISCIPLINA-LPA/exercicio3-estruturaderepeticao.py
@@ -0,0 +1,13 @@
+#Um cinema cobra preços diferentes para os ingressos de acordo com a idade de uma 
+#pessoa. Se a pessoa tiver menos de 3 anos de idade, o ingressa será gratuito, se tiver
+#entre 3 e 12 anos, o ingresso custará R$15, se tiver mais de 12 anos, custará R$ 30
+#Primeiro:
+#Escreva um laço em que vocẽ pergunte a idade dos usuários, e então, informe-lhes o preço
+#dos ingressos do cinema.
+#Segundo: 
+#Encerre o laço usando um break quando o usuário digitar sair.
+#Terceiro:
+#Após encerrar o laço, apresente na tela o total de pessoas que compraram ingressos, o total de dinheiro
+#arrecadado e a média de idade das pessoas. 
+
+print("_________"*30)
