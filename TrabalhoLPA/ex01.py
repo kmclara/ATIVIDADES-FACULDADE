693beca4ed7f0cfe54a8ff6861b8463a1086939e
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-02 10:21:40 -0300

    Concertando erros

---
+++ TrabalhoLPA/ex01.py
@@ -0,0 +1,12 @@
+
+print("ETAPAS DE ENSINO!")
+
+while True:
+    NomeDaCriança = str(input("Qual o nome do estudante? "))
+    IdadeDaCriança = int(input("Qual a idade da criança? "))
+
+    if IdadeDaCriança <=5:
+        print("A aluna {} tem {} e está na Educação Infantil.".format(NomeDaCriança, IdadeDaCriança))
+    
+    
+        
