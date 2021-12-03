b45cabcba50427e63c837b33ef61263cda055b49
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-03 11:06:26 -0300

    Etapas de ensino - exercicio 1
    Neste exercicio foi pedido um programa que apartir da idade do aluno, o programa voltaria em que tipo de ensino ele esta e no final, perguntara se ele deseja continuar ou sair do programa
    Foi utilizado o laço e repetiçao while seguido das duas variaveis perguntando o nome e a idade do aluno/criança
    Logo em seguida, tem as condicionais com as idades e o que deve ser voltado
    Logo em seguida, tem a variavel de pergunta de saida ou nao do usuario, com sua condicional e break caso o usuario escolha a opçao 1

--- TrabalhoLPA/ex01.py
+++ TrabalhoLPA/ex01.py
@@ -6,7 +6,17 @@ while True:
     IdadeDaCriança = int(input("Qual a idade da criança? "))
 
     if IdadeDaCriança <=5:
-        print("A aluna {} tem {} e está na Educação Infantil.".format(NomeDaCriança, IdadeDaCriança))
-    
+        print("O aluno(a) {} tem {} e está na Educação Infantil.".format(NomeDaCriança, IdadeDaCriança))
+    elif IdadeDaCriança >= 6 and IdadeDaCriança <=10:
+        print("O aluno(a) {} tem {} anos e está no Ensino Fundamental 1.".format(NomeDaCriança, IdadeDaCriança))
+    elif IdadeDaCriança >=11 and IdadeDaCriança <=14 :
+        print("O aluno(a) {} tem {} anos e está no Ensino Fundamental 2.".format(NomeDaCriança, IdadeDaCriança))
+    elif IdadeDaCriança >=15:
+        print("O aluno(a) {} tem {} anos e está no Ensino Médio.".format(NomeDaCriança, IdadeDaCriança))
+
+    EscolhaDoUsuário = (input("Deseja sair? 1- SIM 2- NÃO"))
+    if EscolhaDoUsuário == "1":
+        break
+
     
         
