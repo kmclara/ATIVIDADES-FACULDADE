851e92c877326a9c1ec653a16fd59aa471cb0409
Maria Clara Brito Santana <88041461+kmclara@users.noreply.github.com>
2021-12-08 21:41:56 -0300

    arrumando os caracteres das variáveis
    Caracteres das variáveis foram arrumados. Retirados os "ç", alteração de letras no início das variáveis e redundância no input.

--- TrabalhoLPA/ex01.py
+++ TrabalhoLPA/ex01.py
@@ -2,20 +2,20 @@
 print("ETAPAS DE ENSINO!")
 
 while True:
-    NomeDaCriança = str(input("Qual o nome do estudante? "))
-    IdadeDaCriança = int(input("Qual a idade da criança? "))
+    nomeDaCrianca = input("Qual o nome do estudante? ")
+    idadeDaCrianca = int(input("Qual a idade da criança? "))
 
-    if IdadeDaCriança <=5:
-        print("O aluno(a) {} tem {} e está na Educação Infantil.".format(NomeDaCriança, IdadeDaCriança))
-    elif IdadeDaCriança >= 6 and IdadeDaCriança <=10:
-        print("O aluno(a) {} tem {} anos e está no Ensino Fundamental 1.".format(NomeDaCriança, IdadeDaCriança))
-    elif IdadeDaCriança >=11 and IdadeDaCriança <=14 :
-        print("O aluno(a) {} tem {} anos e está no Ensino Fundamental 2.".format(NomeDaCriança, IdadeDaCriança))
-    elif IdadeDaCriança >=15:
-        print("O aluno(a) {} tem {} anos e está no Ensino Médio.".format(NomeDaCriança, IdadeDaCriança))
+    if idadeDaCrianca <=5:
+        print("O aluno(a) {} tem {} e está na Educação Infantil.".format(nomeDaCrianca, idadeDaCrianca))
+    elif idadeDaCrianca >= 6 and idadeDaCrianca <=10:
+        print("O aluno(a) {} tem {} anos e está no Ensino Fundamental 1.".format(nomeDaCrianca, idadeDaCrianca))
+    elif idadeDaCrianca >=11 and idadeDaCrianca <=14 :
+        print("O aluno(a) {} tem {} anos e está no Ensino Fundamental 2.".format(nomeDaCrianca, idadeDaCrianca))
+    elif idadeDaCrianca >=15:
+        print("O aluno(a) {} tem {} anos e está no Ensino Médio.".format(nomeDaCrianca, idadeDaCrianca))
 
-    EscolhaDoUsuário = (input("Deseja sair? 1- SIM 2- NÃO"))
-    if EscolhaDoUsuário == "1":
+    escolhaDoUsuario = (input("Deseja sair? 1- SIM 2- NÃO"))
+    if escolhaDoUsuario == "1":
         break
 
     
