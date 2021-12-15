0ec12ea91d435b924705f86699b28986bf3a824e
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-15 09:09:42 -0300

    Fazendo uma condicional
    Fazendo uma pequena condicional com javascript sobre idade maior ou igual a 18.

--- ExerciciosJS/script.js
+++ ExerciciosJS/script.js
@@ -1,27 +1,39 @@
 //VERSÃO MODERNA JS
 //Declarando variavel vazia:
-var sobrenome;
+//-var sobrenome;
 
 //Atribuindo valor a ela depois:
-sobrenome = "Maria";
+//-sobrenome = "Maria";
 
 //Variaveis com const e let:
 
-const animal = "Cachorro"; 
-let cor = "azul";
+//-const animal = "Cachorro"; 
+//-let cor = "azul";
 
 //Mensagens combinadas:
-console.log("Ola" + nome);
-console.log("A" + nome + "gosta da cor" + cor)
+//-console.log("Ola" + nome);
+//-console.log("A" + nome + "gosta da cor" + cor)
 //Fazendo combinação sem a string, tudo dentro das aspas: Para isso usamos ${nome da variaveis}
-let mensagem = 'Olá ${nome}.';
-console.log(mensagem)
+//-let mensagem = 'Olá ${nome}.';
+//-console.log(mensagem)
 //_____________________________________
 
 //Variavel em JS:
-var nome = "Maria";
+//-var nome = "Maria";
 //Mudando o valor da variavel: 
-nome = "Clara";
+//-nome = "Clara";
 //Imprimindo o valor da varival na tela:
-console.log(nome);
+//-console.log(nome);
 
+let idade = 30;
+let nome = "Maria";
+
+if(idade >= 18){
+    console.log("Seja bem vindo")
+}
+else{
+    console.log("Acesso bloqueado")
+}
+
+//let mensagem = "Olá ${nome}"
+//console.log(mensagem)
