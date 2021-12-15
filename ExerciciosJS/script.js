6c1c1631144c9bfdb1f88181bb4ba4f5ebe8b213
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-15 08:59:12 -0300

    Adicionando formas de descrever variaveis no JS
    Formas: com var, criando uma variaveis porem ser valor e atribuindo valor a ela depois, const e let.

--- ExerciciosJS/script.js
+++ ExerciciosJS/script.js
@@ -1,5 +1,16 @@
-//console.log("Arquivo JS");
+//VERSÃO MODERNA JS
+//Declarando variavel vazia:
+var sobrenome;
 
+//Atribuindo valor a ela depois:
+sobrenome = "Maria";
+
+//Variaveis com const e let:
+
+const animal = "Cachorro"; 
+let cor = "azul";
+
+//_____________________________________
 
 //Variavel em JS:
 var nome = "Maria";
@@ -7,3 +18,4 @@ var nome = "Maria";
 nome = "Clara";
 //Imprimindo o valor da varival na tela:
 console.log(nome);
+
