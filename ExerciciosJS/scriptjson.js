b9f513a90e55319e1c03d89b15dba7a6ddadeb19
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-16 08:46:08 -0300

    Funcionalidades da variavel objeto
    Alguns exemplos de como usar e manipular conteudos das variaveis objetos.

---
+++ ExerciciosJS/scriptjson.js
@@ -0,0 +1,10 @@
+let pessoa = { nome: "Maria",
+               idade: 30};
+
+//pedindo só o nome de dentro da variavel objeto:
+console.log(pessoa.nome);
+//ou:
+console.log(pessoa["nome"]);
+
+//para mudar o valor de dentro da variavel: 
+pessoa.nome= "Mateus"
