d9ca060dbbbe33fa8c9a30c410dead56d636f608
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-16 09:54:06 -0300

    Adicionando funçao de evento e criando uma funçao
    Adicionando funçao addEventListener, onde ao passar no mouse, ele ficara verde.
    Criando a funçao mudaVerde para que coloca os codigos que mudarao a cor para verde quando se passado o mouse.

--- ExerciciosJS/script1.js
+++ ExerciciosJS/script1.js
@@ -1,2 +1,8 @@
 let botão = document.querySelector("#botão");
 botão.style.background="blue";
+
+botão.addEventListener("mouseover", mudaVerde);
+
+function mudaVerde(){
+    botão.style.background="green";
+}
