d7025a8c7929db551aa757627a19019be2aac021
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-16 10:04:42 -0300

    Criando mais uma funçao
    Criando mais uma funçao para quando for clicado, ficar vermelho e mostrar a mensagem Quebrei

--- ExerciciosJS/script1.js
+++ ExerciciosJS/script1.js
@@ -12,3 +12,10 @@ botão.addEventListener("mouseout", mudaAzul);
 function mudaAzul(){
     botão.style.background="blue"
 }
+
+botão.addEventListener("click", mudaVermelho);
+
+function mudaVermelho(){
+    botão.style.background="red";
+    botão.innerHTML="Quebrei!";
+}
