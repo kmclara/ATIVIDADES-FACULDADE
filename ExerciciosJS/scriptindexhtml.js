22e15056a5bc9cef314585470366afe424afd374
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-12-16 09:34:25 -0300

    Adicionando funçao de evento
    Adicionando funçao addEventListener para adicionarmos um evento no paragrafoum.
    Foi feito uma funçao chamada trocaTexto onde se clicava em cima do mouse e ele mudava de cor, cor escolhida neste caso o verde - green/'

--- ExerciciosJS/scriptindexhtml.js
+++ ExerciciosJS/scriptindexhtml.js
@@ -1,3 +1,9 @@
 let paragrafo = document.querySelector("#paragrafoum");
 
 //adicionando evento ao paragrafo:
+paragrafo.addEventListener("click", trocaTexto);
+
+function trocaTexto(){
+    paragrafoum.style.background="green";
+
+}
